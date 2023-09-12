1b123e27d5439285322b369f1e99eb65eefe588f
Your Name <youremail@domain.com>
2023-09-12 20:45:21 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'; // 2.1引入结构
+import 'element-ui/lib/theme-chalk/index.css'; // 2.2引入样式
+import 'default-passive-events'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI); // 3.安装
 new Vue({
   router,
   render: h => h(App)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <el-table :data="items" border style="width: 100%">
+      <el-table-column
+          prop="yuwen"
+          label="语文"
+          width="180"
+      ></el-table-column>
+      <el-table-column
+          prop="shuxue"
+          label="数学"
+          width="180"
+      ></el-table-column>
+      <el-table-column prop="yinyu" label="英语" width="180"></el-table-column>
+      <el-table-column>
+        <el-button  @click="centerDialogVisible = true">修改</el-button>
+        <el-button>删除</el-button>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog
+        title="提示"
+        :visible.sync="centerDialogVisible"
+        @update:visible="handleDialogUpdate"
+        width="30%"
+        center
+        @close="handleDialogClose"
+    >
+      <div>
+        <label>语文：</label>
+        <el-input size="mini" v-model="yuwen"></el-input>
+      </div>
+      <div>
+        <label>数学：</label>
+        <el-input size="mini" v-model="shuxue"></el-input>
+      </div>
+      <div>
+        <label>英语：</label>
+        <el-input size="mini" v-model="yinyu"></el-input>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="centerDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      items: [],
+      centerDialogVisible: false,
+      yuwen:0,
+      shuxue:0,
+      yinyu:0
+    };
+  },
+
+  mounted() {
+    this.select();
+  },
+
+  methods: {
+    handleDialogUpdate(value) {
+      console.log('Dialog visibility changed:', value);
+    },
+
+    handleDialogClose() {
+      // Reload the page when the dialog is closed
+      window.location.reload();
+    },
+
+    select() {
+      axios
+          .get('http://localhost:8080/sorce/select')
+          .then((response) => {
+            this.items = response.data;
+            console.log(this.items);
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          });
+    },
+  },
+};
 </script>
+
