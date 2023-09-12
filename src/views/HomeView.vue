0dae6850c0aefe04a1332b82d6678f92a9d76849
Your Name <youremail@domain.com>
2023-09-12 23:44:43 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div>
     <el-table :data="items" border style="width: 100%">
       <el-table-column
@@ -13,19 +12,17 @@
           width="180"
       ></el-table-column>
       <el-table-column prop="yinyu" label="英语" width="180"></el-table-column>
-      <el-table-column>
-        <el-button  @click="centerDialogVisible = true">修改</el-button>
-        <el-button>删除</el-button>
-      </el-table-column>
+      < <el-table-column>
+      <el-button @click="openEditDialog">修改</el-button>
+      <el-button @click="deleteItem">删除</el-button>
+    </el-table-column>
     </el-table>
 
     <el-dialog
         title="提示"
         :visible.sync="centerDialogVisible"
-        @update:visible="handleDialogUpdate"
         width="30%"
         center
-        @close="handleDialogClose"
     >
       <div>
         <label>语文：</label>
@@ -40,11 +37,10 @@
         <el-input size="mini" v-model="yinyu"></el-input>
       </div>
       <span slot="footer" class="dialog-footer">
-    <el-button @click="centerDialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
-  </span>
+        <el-button @click="centerDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="saveChanges">确定</el-button>
+      </span>
     </el-dialog>
-
   </div>
 </template>
 
@@ -56,9 +52,10 @@ export default {
     return {
       items: [],
       centerDialogVisible: false,
-      yuwen:0,
-      shuxue:0,
-      yinyu:0
+      yuwen: 0,
+      shuxue: 0,
+      yinyu: 0,
+      selectedItem: null, // 选中的数据项
     };
   },
 
@@ -67,18 +64,37 @@ export default {
   },
 
   methods: {
-    handleDialogUpdate(value) {
-      console.log('Dialog visibility changed:', value);
+    // 打开编辑对话框，并传递选中的数据
+    openEditDialog(item) {
+      this.selectedItem = item;
+      this.yuwen = item.yuwen;
+      this.shuxue = item.shuxue;
+      this.yinyu = item.yinyu;
+      this.centerDialogVisible = true;
     },
 
-    handleDialogClose() {
-      // Reload the page when the dialog is closed
-      window.location.reload();
+    // 删除数据项的方法
+    deleteItem() {
+      // 实现删除逻辑，你可以在这里使用axios发送删除请求
     },
 
+    // 保存修改的方法
+    saveChanges() {
+      // 执行保存修改的逻辑，可以使用axios发送保存请求
+      // 这里假设要修改的数据已经保存成功
+
+      axios.post("")
+      // 刷新数据
+      this.select();
+
+      // 关闭对话框
+      this.centerDialogVisible = false;
+    },
+
+    // 获取数据的方法
     select() {
       axios
-          .get('http://localhost:8080/sorce/select')
+          .get('http://10.100.255.51:8080/sorce/select')
           .then((response) => {
             this.items = response.data;
             console.log(this.items);
@@ -87,7 +103,9 @@ export default {
             console.error('Error fetching data:', error);
           });
     },
+    update(){
+
+    }
   },
 };
 </script>
-
